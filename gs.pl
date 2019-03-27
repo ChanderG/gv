2282b2e6512ee15725b0b8764bfa82d896527594
ChanderG <mail@chandergovind.org>
2019-03-27 14:43:24 +0530

    display commit data

--- gs.pl
+++ gs.pl
@@ -3,8 +3,8 @@ use Mojolicious::Lite;
 
 get '/commits' => sub {
 	my $self = shift;
-
 	my $result = [];
+
 	my $rawout = `git log --format="format:%ad, %an, %s, %H"`;
 	my @commits = split /\n/, $rawout;
 	foreach my $commit (@commits) {
@@ -19,6 +19,16 @@ get '/commits' => sub {
 
 	$self->res->headers->header('Access-Control-Allow-Origin' => '*');
 	$self->render(json => $result);
+};
+
+get '/commit/:commitid' => sub {
+	my $self = shift;
+	my $commitid = $self->stash('commitid');
+	my $result = [];
+
+	my $result = `git show $commitid`;
+	$self->res->headers->header('Access-Control-Allow-Origin' => '*');
+	$self->render(text => $result);
 };
 
 app->start;
--- src/components/CommitViewer.vue
+++ src/components/CommitViewer.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
     <h2>Commit Viewer</h2>
-    <h3> {{ this.$store.state.currhash }} </h3>
+    <h3> {{ currHash }} </h3>
+    <div class="commitdetails"> {{ details }} </div>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+
 export default {
-  name: 'CommitViewer'
+    name: 'CommitViewer',
+    data: function() {
+        return {
+            details: ""
+        }
+    },
+    computed: {
+        currHash: function () {
+            return this.$store.state.currhash;
+        }
+    },
+    watch: {
+        currHash: function (val) {
+            this.details = "loading"
+            var vm = this
+            axios.get("http://localhost:3000/commit/" + val).then(response => (vm.details = response.data))
+        }
+    }
 }
 </script>
+
+<style>
+.commitdetails {
+  white-space: pre-wrap;
+  text-align: left;
+  margin: 10px;
+  font-size: large;
+}
+</style>
